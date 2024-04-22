fa92f4e51317488c31c45c389ef1e0a4fcefc67e
Thái Bảo Long <subin27102003@gmail.com>
2024-04-22 23:42:11 +0700

    Remove buyBook route from buybook.route.js

--- routes/buybook.route.js
+++ routes/buybook.route.js
@@ -1,5 +1,1 @@
-import { buyBook } from "../controllers/bookController.js";
-import { Router } from "express";
-const router = Router();
-router.route("/payment").post(buyBook);
-export const bookRouter = router;
+
