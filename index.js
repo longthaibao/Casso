13c95696a96ae7c02521ce71e7c5c40c9d148ee7
Thái Bảo Long <subin27102003@gmail.com>
2024-04-23 13:17:20 +0700

    Update return URL in create-payment-link route

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ app.get("/create-payment-link", async (req, res) => {
       amount: 10000,
       description: "Bí mật của may mắn",
       orderCode: Number(String(Date.now()).slice(-6)),
-      returnUrl: "https://casso.vercel-rqt8.app/dowloadbook",
+      returnUrl: "https://casso-rqt8.vercel.app/dowloadbook",
       cancelUrl: "https://casso-rqt8.vercel.app/",
       signature: "string",
     };
